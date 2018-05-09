a9c0f309d39057b19612253d0bba593d53b7438e
vanessa-hughes <vanessa.hughes@onceit.co.nz>
2018-05-10 11:00:30 +1200

    Configure webpack with sass and js

--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,17 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('app', ['babel-polyfill', 'whatwg-fetch', './assets/js/entryPoint.js'])
+    .addStyleEntry('css/main', './assets/sass/layout.scss')
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
