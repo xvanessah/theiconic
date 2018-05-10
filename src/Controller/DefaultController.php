929370fe67bf4978b79b17a0345b17c05b6c8d07
vanessa-hughes <vanessa.hughes@onceit.co.nz>
2018-05-10 12:04:04 +1200

    Forgot these use statements

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 use GuzzleHttp\Client;
 
